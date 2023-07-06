f90e3d7b01a9d586313b861efe67f5863be3c56a
Chiragmodi01 <chiragmodi2001@gmail.com>
2023-07-06 23:56:54 +0530

    feat: 🎸 CORS enabled

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const responseData = require("./data.json");
+const cors = require("cors");
 
 const app = express();
+
+// Enable CORS for all routes
+app.use(cors());
 
 app.get("/", (req, res) => {
   const data = responseData;
